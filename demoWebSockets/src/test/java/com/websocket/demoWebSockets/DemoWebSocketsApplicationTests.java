210f7e802de0313440da748bea49badf4ec59cda
Jae Dreijling <j.dreijling@student.han.nl>
2025-02-04 14:00:28 +0100

    added spring initializer

---
+++ demoWebSockets/src/test/java/com/websocket/demoWebSockets/DemoWebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.demoWebSockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
